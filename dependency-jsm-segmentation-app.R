066c005d80e0edb9525c4a35f92936b889f2213c
yogesh1612 <yogesh1612@users.noreply.github.com>
2019-10-11 20:07:31 +0530

    Add files via upload

---
+++ dependency-jsm-segmentation-app.R
@@ -0,0 +1,6 @@
+try(require(shiny) || install.packages("shiny"))
+try(require(kableExtra) || install.packages("kableExtra"))
+try(require(shinyFeedback) || install.packages("shinyFeedback"))
+try(require(dplyr) || install.packages("dplyr"))
+#try(require(shiny) || install.packages("shiny"))
+
