cad5d75c68caeb22a5dfc2ea197e93895f478f56
yogesh1612 <yogesh1612@users.noreply.github.com>
2019-10-14 22:24:38 +0530

    Delete dependency-jsm-segmentation-app.R

--- dependency-jsm-segmentation-app.R
+++
@@ -1,6 +0,0 @@
-try(require(shiny) || install.packages("shiny"))
-try(require(kableExtra) || install.packages("kableExtra"))
-try(require(shinyFeedback) || install.packages("shinyFeedback"))
-try(require(dplyr) || install.packages("dplyr"))
-#try(require(shiny) || install.packages("shiny"))
-
